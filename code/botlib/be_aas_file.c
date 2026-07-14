rorgoroth/Quake3e

commit d6f26c2efed1b3537629108b54cf981e5259e435

network: fixed incorrect timeout parameter for NET_FlushPacketQueue() win32: added netmask detection function for ipv4 addresses misc: fixed various compiler warnings

code/botlib/be_aas_file.c
@@ -218,18 +218,19 @@ static const char *AAS_ValidateAASData(void)
 	}
 
 	for (i = 0; i < aasworld.numareasettings; i++) {
+		int v;
 		if (CHECK_RANGE(aasworld.areasettings[i].firstreachablearea,
 						aasworld.areasettings[i].numreachableareas,
 						aasworld.reachabilitysize))
 			return "areasettings: bad reachable areas";
-		c = aasworld.areasettings[i].cluster;
-		if (c & 0x80000000) {
-			if (-c >= aasworld.numportals)
+		v = aasworld.areasettings[i].cluster;
+		if (v < 0) {
+			if (-v >= aasworld.numportals)
 				return "areasettings: bad portal";
 		} else {
-			if (c >= aasworld.numclusters)
+			if (v >= aasworld.numclusters)
 				return "areasettings: bad cluster";
-			if ((unsigned)aasworld.areasettings[i].clusterareanum >= (c ? aasworld.clusters[c].numareas : 1))
+			if ((unsigned)aasworld.areasettings[i].clusterareanum >= (v ? aasworld.clusters[c].numareas : 1))
 				return "areasettings: bad clusterareanum";
 		}
 	}
@@ -249,12 +250,12 @@ static const char *AAS_ValidateAASData(void)
 		if ((unsigned)aasworld.nodes[i].planenum >= aasworld.numplanes)
 			return "nodes: bad planenum";
 		for (j = 0; j < 2; j++) {
-			c = aasworld.nodes[i].children[j];
-			if (c & 0x80000000) {
-				if (-c >= aasworld.numareasettings)
+			const int v = aasworld.nodes[i].children[j];
+			if ( v < 0 ) {
+				if (-v >= aasworld.numareasettings)
 					return "nodes: bad areasetting";
 			} else {
-				if (c >= aasworld.numnodes)
+				if (v >= aasworld.numnodes)
 					return "nodes: bad node";
 			}
 		}
@@ -442,7 +443,7 @@ static char *AAS_LoadAASLump(fileHandle_t fp, long offset, unsigned length, long
 	//allocate memory
 	buf = (char *) GetClearedHunkMemory(length);
 	//read the data
-	if (botimport.FS_Read(buf, length, fp) != length) {
+	if (botimport.FS_Read(buf, length, fp) != (int)length) {
 		AAS_Error("can't read AAS file\n");
 		AAS_DumpAASData();
 		botimport.FS_FCloseFile(fp);

code/qcommon/msg.c
@@ -219,7 +219,7 @@ static int MSG_ReadBits( msg_t *msg, int bits ) {
 
 	if ( sgn && bits < 32 ) {
 		if ( value & ( 1 << ( bits - 1 ) ) ) {
-			value |= -1 ^ ( ( 1 << bits ) - 1 );
+			value |= -1 ^ ( ( 1U << bits ) - 1 );
 		}
 	}
 
@@ -290,7 +290,7 @@ void MSG_WriteString( msg_t *sb, const char *s ) {
 
 	for ( i = 0 ; i < l; i++ ) {
 		// get rid of 0x80+ and '%' chars, because old clients don't like them
-		if ( s[i] & 0x80 || s[i] == '%' )
+		if ( (byte)s[i] & 0x80 || s[i] == '%' )
 			v = '.';
 		else
 			v = s[i];
@@ -312,7 +312,7 @@ void MSG_WriteBigString( msg_t *sb, const char *s ) {
 
 	for ( i = 0 ; i < l ; i++ ) {
 		// get rid of 0x80+ and '%' chars, because old clients don't like them
-		if ( s[i] & 0x80 || s[i] == '%' )
+		if ( (byte)s[i] & 0x80 || s[i] == '%' )
 			v = '.';
 		else
 			v = s[i];

code/qcommon/net_ip.c
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifdef _WIN32
 #	include <winsock2.h>
 #	include <ws2tcpip.h>
+#	include <iphlpapi.h>
 #	if WINVER < 0x501
 #		ifdef __MINGW32__
 			// wspiapi.h isn't available on MinGW, so if it's
@@ -252,7 +253,7 @@ static char *NET_ErrorString( void ) {
 		case WSAENETDOWN: return "WSAENETDOWN";
 		case WSAENETUNREACH: return "WSAENETUNREACH";
 		case WSAENETRESET: return "WSAENETRESET";
-		case WSAECONNABORTED: return "WSWSAECONNABORTEDAEINTR";
+		case WSAECONNABORTED: return "WSWSAECONNABORTED";
 		case WSAECONNRESET: return "WSAECONNRESET";
 		case WSAENOBUFS: return "WSAENOBUFS";
 		case WSAEISCONN: return "WSAEISCONN";
@@ -591,7 +592,7 @@ const char *NET_AdrToString( const netadr_t *a )
 
 const char *NET_AdrToStringwPort( const netadr_t *a )
 {
-	static char s[NET_ADDRSTRMAXLEN];
+	static char s[NET_ADDRSTRMAXLEN + 8]; // including strlen("[]:65535")
 
 	if (a->type == NA_LOOPBACK)
 		strcpy( s, "loopback" );
@@ -1424,14 +1425,14 @@ static void NET_GetLocalAddress( void )
 	char	hostname[256];
 	struct ifaddrs *ifap, *search;
 
+	numIP = 0;
+
 	if ( gethostname( hostname, sizeof( hostname ) ) )
 		return;
 
 	Com_Printf( "Hostname: %s\n", hostname );
 
-	numIP = 0;
-
-	if ( getifaddrs( &ifap ) )
+	if ( getifaddrs( &ifap ) != 0 )
 		Com_Printf( "NET_GetLocalAddress: Unable to get list of network interfaces: %s\n", NET_ErrorString() );
 	else
 	{
@@ -1441,13 +1442,57 @@ static void NET_GetLocalAddress( void )
 			if ( ifap->ifa_flags & IFF_UP )
 				NET_AddLocalAddress( search->ifa_name, search->ifa_addr, search->ifa_netmask );
 		}
-	
+
 		freeifaddrs( ifap );
-		
+
 		Sys_ShowIP();
 	}
 }
 #else // _WIN32
+
+static void NET_FindNetMask4( const struct sockaddr *addr, struct sockaddr *netmask )
+{
+	MIB_IPADDRTABLE *addrTable;
+	const struct sockaddr_in *s;
+	DWORD dwResult, i;
+	HANDLE hHeap;
+	ULONG size;
+
+	size = 0;
+	dwResult = GetIpAddrTable( NULL, &size, 0 );
+	if ( dwResult != ERROR_INSUFFICIENT_BUFFER ) {
+		return;  // keep original netmask
+	}
+
+	hHeap = GetProcessHeap();
+	if ( hHeap == NULL ) {
+		return;
+	}
+	addrTable = (MIB_IPADDRTABLE*)HeapAlloc( hHeap, 0, size );
+	if ( addrTable == NULL ) {
+		return;
+	}
+
+	dwResult = GetIpAddrTable( addrTable, &size, 0 );
+	if ( dwResult != NO_ERROR ) {
+		HeapFree( hHeap, 0, addrTable );
+		return;
+	}
+
+	s = (const struct sockaddr_in *)addr;
+
+	for ( i = 0; i < addrTable->dwNumEntries; i++ ) {
+		if ( memcmp( &s->sin_addr, &addrTable->table[i].dwAddr, sizeof( IN_ADDR ) ) == 0 ) {
+			struct sockaddr_in *o = (struct sockaddr_in *)netmask;
+			memcpy( &o->sin_addr, &addrTable->table[i].dwMask, sizeof( IN_ADDR ) );
+			break;
+		}
+	}
+
+	HeapFree( hHeap, 0, addrTable );
+}
+
+
 static void NET_GetLocalAddress( void ) {
 	char	hostname[256];
 	struct addrinfo	hint;
@@ -1459,29 +1504,29 @@ static void NET_GetLocalAddress( void ) {
 		return;
 
 	Com_Printf( "Hostname: %s\n", hostname );
-	
+
 	memset(&hint, 0, sizeof(hint));
-	
+
 	hint.ai_family = AF_UNSPEC;
 	hint.ai_socktype = SOCK_DGRAM;
-	
-	if ( !getaddrinfo( hostname, NULL, &hint, &res ) )
+
+	if ( getaddrinfo( hostname, NULL, &hint, &res ) == 0 )
 	{
-		struct addrinfo *search;
+		const struct addrinfo *search;
 		struct sockaddr_in mask4;
 #ifdef USE_IPV6
 		struct sockaddr_in6 mask6;
 #endif
-	
+
 		/* On operating systems where it's more difficult to find out the configured interfaces, we'll just assume a
 		 * netmask with all bits set. */
-	
-		memset(&mask4, 0, sizeof(mask4));
+
+		memset(&mask4, 0x0, sizeof(mask4));
 		mask4.sin_family = AF_INET;
 		memset(&mask4.sin_addr.s_addr, 0xFF, sizeof(mask4.sin_addr.s_addr));
 
 #ifdef USE_IPV6
-		memset(&mask6, 0, sizeof(mask6));
+		memset(&mask6, 0x0, sizeof(mask6));
 		mask6.sin6_family = AF_INET6;
 		memset(&mask6.sin6_addr, 0xFF, sizeof(mask6.sin6_addr));
 #endif
@@ -1489,17 +1534,23 @@ static void NET_GetLocalAddress( void ) {
 		// add all IPs from returned list.
 		for ( search = res; search; search = search->ai_next )
 		{
-			if ( search->ai_family == AF_INET )
-				NET_AddLocalAddress( "", search->ai_addr, (struct sockaddr *) &mask4 );
+			if ( search->ai_family == AF_INET ) {
+				NET_FindNetMask4( search->ai_addr, (struct sockaddr*)&mask4 );
+				NET_AddLocalAddress( "", search->ai_addr, (struct sockaddr*)&mask4 );
+				continue;
+			}
 #ifdef USE_IPV6
-			else if ( search->ai_family == AF_INET6 )
-				NET_AddLocalAddress( "", search->ai_addr, (struct sockaddr *) &mask6 );
+			if ( search->ai_family == AF_INET6 ) {
+				// TODO: NET_FindNetMask6()
+				NET_AddLocalAddress( "", search->ai_addr, (struct sockaddr*)&mask6 );
+				continue;
+			}
 #endif
 		}
-	
+
 		Sys_ShowIP();
 	}
-	
+
 	if ( res )
 		freeaddrinfo( res );
 }
@@ -1691,6 +1742,8 @@ static void NET_Config( qboolean enableNetworking ) {
 	qboolean	modified;
 	qboolean	stop;
 	qboolean	start;
+
+	NET_FlushPacketQueue( -99999 );
 
 	// get any latched changes to cvars
 	modified = NET_GetCvars();

code/qcommon/q_shared.c
@@ -743,7 +743,7 @@ char *COM_ParseComplex( const char **data_p, qboolean allowLineBreaks )
 	str = (byte*)*data_p;
 	len = 0; 
 	shift = 0; // token line shift relative to com_lines
-	com_tokentype = TK_GENEGIC;
+	com_tokentype = TK_GENERIC;
 	
 __reswitch:
 	switch ( *str )

code/qcommon/q_shared.h
@@ -63,7 +63,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //#pragma warning(disable : 4136)
 #pragma warning(disable : 4152)		// nonstandard extension, function/data pointer conversion in expression
 #pragma warning(disable : 4200)		// nonstandard extension used: size-sided array in struct/union
-//#pragma warning(disable : 4201)
+#pragma warning(disable : 4201)		// nonstandard extension used: nameless struct/union
 #pragma warning(disable : 4206)		// nonstandard extension used: translation unit is empty
 //#pragma warning(disable : 4214)
 #pragma warning(disable : 4267)		// conversion from 'size_t' to 'int', possible loss of data
@@ -749,7 +749,7 @@ void	COM_ParseWarning( const char *format, ... ) __attribute__ ((format (printf,
 char	*COM_ParseComplex( const char **data_p, qboolean allowLineBreak );
 
 typedef enum {
-	TK_GENEGIC = 0, // for single-char tokens
+	TK_GENERIC = 0, // for single-char tokens
 	TK_STRING,
 	TK_QUOTED,
 	TK_EQ,

code/qcommon/qcommon.h
@@ -177,7 +177,11 @@ typedef enum {
 } netsrc_t;
 
 
-#define NET_ADDRSTRMAXLEN 48	// maximum length of an IPv6 address string including trailing '\0'
+#ifdef USE_IPV6
+#define NET_ADDRSTRMAXLEN 64	// maximum length of an IPv6 address string including trailing '\0'
+#else
+#define NET_ADDRSTRMAXLEN 16	// maximum length of an IPv4 address string including trailing '\0'
+#endif
 
 typedef struct {
 	netadrtype_t	type;

code/renderer/tr_shader.c
@@ -1578,7 +1578,7 @@ static qboolean ParseCondition( const char **text, resultType *res )
 	for ( ;; )
 	{
 		rval_str[0] = '\0';
-		rval_type = TK_GENEGIC;
+		rval_type = TK_GENERIC;
 
 		// expect l-value at least
 		token = COM_ParseComplex( text, qfalse );

code/renderervk/tr_shader.c
@@ -1575,7 +1575,7 @@ static qboolean ParseCondition( const char **text, resultType *res )
 	for ( ;; )
 	{
 		rval_str[0] = '\0';
-		rval_type = TK_GENEGIC;
+		rval_type = TK_GENERIC;
 
 		// expect l-value at least
 		token = COM_ParseComplex( text, qfalse );

commit 68c21fe10663ab909a17ec9972bfa77d54d85df0

botlib: stale variable in AAS_ValidateAASData causes false "bad clusterareanum" errors (regression in d6f26c2)

code/botlib/be_aas_file.c
@@ -230,7 +230,7 @@ static const char *AAS_ValidateAASData(void)
 		} else {
 			if (v >= aasworld.numclusters)
 				return "areasettings: bad cluster";
-			if ((unsigned)aasworld.areasettings[i].clusterareanum >= (v ? aasworld.clusters[c].numareas : 1))
+			if ((unsigned)aasworld.areasettings[i].clusterareanum >= (v ? aasworld.clusters[v].numareas : 1))
 				return "areasettings: bad clusterareanum";
 		}
 	}